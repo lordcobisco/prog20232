71565c976b0080678f910554aa061db09d12d453
Gustavo Maciel <79233238+Gus-1003@users.noreply.github.com>
2023-09-03 23:53:10 -0300

    Update 0_helloword.py

--- Gustavo/Praticas/0_helloword.py
+++ Gustavo/Praticas/0_helloword.py
@@ -1,1 +1,11 @@
+# Primeiro código escrito na disciplina de "Fundamentos de Programação e Desenvolvimento de Projetos aplicados a Neuroengenharia"
+'''
+Turma: 2023.2
+Professor: Andre Dantas
+Aluno: Gustavo Maciel
+
+Atividade: 1
+Aula: 3
+'''
+ 
 print("Hello Word")
