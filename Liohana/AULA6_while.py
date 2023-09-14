a9bfe0ae9e229e5fe3c22b923db69bb7add9c6ad
Liohana <142513545+Liohana@users.noreply.github.com>
2023-09-14 11:28:32 -0300

    Create AULA6_while.py

---
+++ Liohana/AULA6_while.py
@@ -0,0 +1,12 @@
+### Comando de repetição: while
+
+acabouProg = False
+
+while not acabouProg:
+    acabouProg = input() == "x"
+    pass
+
+while True:
+    if input() == "x":
+        break
+    pass
