2084a580391086f21f533ba693bb262cb668766b
BeatrizOlobardi <142513619+BeatrizOlobardi@users.noreply.github.com>
2023-09-14 11:47:45 -0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,24 @@
+#TempoExperimento = 1 #s
+#frequenciaSensor = 10 #Hz
+#qtdDados = TempoExperimento*frequenciaSensor
+#coleta
+#dado = []
+#for contador in range(qtdDados):
+#    dado.append(float(input()))
+#print(dado)
+
+# dado = [89127, 1298, 902, 3097, 356]
+soma = 0
+#for contador in range(len(dado)):
+#    soma = soma + (1 / frequenciaSensor) * dado[contador]
+#for data in dado:
+#    soma = soma + (1 / frequenciaSensor) * data
+#print(soma)
+
+dicionario = {
+                "Brasil": [1, 2, 2, 1, 4, 5, 3], 
+                "EUA": [3, 4, 56, 3, 6, 4, 2,]
+                }
+for pais in dicionario:
+    print(pais)
+    print(dicionario[pais])
