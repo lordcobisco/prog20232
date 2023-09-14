fab79a562e1d4f1698e5071f460ddf5228803be4
Lucivanio Varela da Silva <142513532+Lucivaniovarela@users.noreply.github.com>
2023-09-14 12:02:34 -0300

    REPRODUÇÃO DOS PROGRAMAS NA - AULA 6

---
+++ Lucivanio Varela/AULA 6/while.py
@@ -0,0 +1,21 @@
+
+# acabouPrograma = False
+# while True:
+#     acabouPrograma = (input () == 'x')
+#     pass 
+
+
+# # forma 1
+
+acabouPrograma = False
+while not acabouPrograma:
+    acabouPrograma = input() == "x"
+    pass 
+print("acabou")
+
+# while True:
+#     print("digite algo e se quiser sair digite x")
+#     if input() == "x":
+#         break
+#     pass
+# print("acabouPrograma")
