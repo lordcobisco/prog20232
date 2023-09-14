adb7b038b51dff4b1c9488e4282960b08142063d
Giovanna Rodrigues <142513161+gihbs@users.noreply.github.com>
2023-09-14 11:49:49 -0300

    Estrutura de repetição condicional

---
+++ Giovanna/codigosAulas/while.py
@@ -0,0 +1,10 @@
+acabouPrograma = False
+
+while not acabouPrograma:
+    acabouPrograma = (input() == "x")
+    pass
+
+while True: 
+    if input() == "x":
+        break # quebra a estrutura de repetição mais próxima; também tem o continue, que voltar pro início da repetição
+    pass
