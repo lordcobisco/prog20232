dc284f3bf8bd26483f445c4dcb69856a32d15e23
BeatrizOlobardi <142513619+BeatrizOlobardi@users.noreply.github.com>
2023-09-17 16:53:50 -0300

    Add files via upload

---
+++ AtividadeContextualizada03.py
@@ -0,0 +1,157 @@
+#variaveis
+
+format = "512x512"
+speed = 400
+pixelSize = 480
+sequencialScan = 1
+zoom = 0
+uv = 30
+rotation = 0
+cor = "azul"
+varredura = "sequencial"
+tipoDeCelula = "AcidoDesoxirribonucleico"
+
+print("Analisar imagens de microscopial confocal")
+
+format = input("Qual o valor Format sera utilizado? :")
+print("Houve alteracao?") 
+print(format!= "512x512")
+
+speed = int(input("Analisar velocidade utilizada?"))
+print("Houve alguma alteracao?")
+print(speed!=400)
+
+pixelSize =int(input("Analisar tamanho de pixels em amostra:"))
+print("Houve alguma alteracao?")
+print(pixelSize!=480)
+
+sequencialScan =int(input("Analisar sequencias:"))
+print("Houve alguma alteracao?")
+print(sequencialScan!=1)
+
+zoom = int(input("Qual a proximidade necessaria para avaliar a amostra?"))
+print("Houve alguma alteracao?")
+print(zoom!=0)
+
+uv = int(input("Qual o tipo de iluminacao necessaria?"))
+print("Houve alguma alteracao?")
+print(uv!=30)
+
+rotation = int(input("Qual o grau de rotação?"))
+print("Houve alguma alteracao?")
+print(rotation!=0)
+
+cor = input("Qual cor sera utilizada?")
+print("houve alguma alteracao?")
+print(cor!="azul")
+
+varredura = input("Qual tipo de varrudura sera utilizada?")
+print("Houve alguma interacao?")
+print(varredura!="sequencial")
+
+tipoDeCelula = input("Qual o tipo de celula analisada?")
+print("Houve alguma alteracao?")
+print(tipoDeCelula!="AcidoDesoxirribonucleico")
+
+print("As informacoes de configuracoes feitas pelo usuario são:")
+print("format=", format)
+print("speed=", speed)
+print("pixelSize=", pixelSize)
+print("sequencialScan=", sequencialScan)
+print("zoom=", zoom)
+print("uv=", uv)
+print("rotation=", rotation)
+print("cor=", cor)
+print("varredura=", varredura)
+print("tipoDeCelula=", tipoDeCelula)
+
+print("Inicio de calibracao do equipamento no sentido horizontal. Use a primeira e ultima letra do seu nome.")
+primeira_letra= "b"
+ultima_letra= "z"
+print("escolha 'b' or 'z'")
+letra1 = input("digite primera ou ultima letra")
+print("informação correta", letra1)
+letra2 = input("digite primera ou ultima letra")
+print("informação correta", letra1)
+letra3 = input("digite primera ou ultima letra")
+print("informação correta", letra3)
+letra4 = input("digite primera ou ultima letra")
+print("informação correta", letra4)
+letra5 = input("digite primera ou ultima letra")
+print("informação correta", letra5)
+letra6 = input("digite primera ou ultima letra")
+print("informação correta", letra6)
+letra7 = input("digite primera ou ultima letra")
+print("informação correta", letra7)
+letra8 = input("digite primera ou ultima letra")
+print("informação correta", letra8)
+letra9 = input("digite primera ou ultima letra")
+print("informação correta", letra9)
+letra10 = input("digite primera ou ultima letra")
+print("informação correta", letra10)
+letra11 = input("digite primera ou ultima letra")
+print("informação correta", letra11)
+letra12 = input("digite primera ou ultima letra")
+print("informação correta", letra12)
+letra13 = input("digite primera ou ultima letra")
+print("informação correta", letra13)
+letra14 = input("digite primera ou ultima letra")
+print("informação correta", letra14)
+letra15 = input("digite primera ou ultima letra")
+print("informação correta", letra15)
+letra16 = input("digite primera ou ultima letra")
+print("informação correta", letra16)
+letra17 = input("digite primera ou ultima letra")
+print("informação correta", letra17)
+letra18 = input("digite primera ou ultima letra")
+print("informação correta", letra18)
+letra19 = input("digite primera ou ultima letra")
+print("informação correta", letra19)
+letra20 = input("digite primera ou ultima letra")
+print("informação correta", letra20)
+
+print("Inicio de calibracao do equipamento no sentido vertical. Use a primeira e ultima letra do seu nome.")
+segunda_letra= "e"
+peultima_letra= "i"
+letra21 = input("digite primera ou ultima letra")
+print("informação correta", letra21)
+letra22 = input("digite primera ou ultima letra")
+print("informação correta", letra22)
+letra23 = input("digite primera ou ultima letra")
+print("informação correta", letra23)
+letra24 = input("digite primera ou ultima letra")
+print("informação correta", letra24)
+letra25 = input("digite primera ou ultima letra")
+print("informação correta", letra25)
+letra26 = input("digite primera ou ultima letra")
+print("informação correta", letra26)
+letra27 = input("digite primera ou ultima letra")
+print("informação correta", letra27)
+letra28 = input("digite primera ou ultima letra")
+print("informação correta", letra28)
+letra29 = input("digite primera ou ultima letra")
+print("informação correta", letra29)
+letra30 = input("digite primera ou ultima letra")
+print("informação correta", letra30)
+letra31 = input("digite primera ou ultima letra")
+print("informação correta", letra31)
+letra32 = input("digite primera ou ultima letra")
+print("informação correta", letra32)
+letra33 = input("digite primera ou ultima letra")
+print("informação correta", letra33)
+letra34 = input("digite primera ou ultima letra")
+print("informação correta", letra34)
+letra35 = input("digite primera ou ultima letra")
+print("informação correta", letra35)
+letra36 = input("digite primera ou ultima letra")
+print("informação correta", letra36)
+letra37 = input("digite primera ou ultima letra")
+print("informação correta", letra37)
+letra38 = input("digite primera ou ultima letra")
+print("informação correta", letra38)
+letra39 = input("digite primera ou ultima letra")
+print("informação correta", letra39)
+letra40 = input("digite primera ou ultima letra")
+print("informação correta", letra40)
+
+print("termino da calibração do sistema")
