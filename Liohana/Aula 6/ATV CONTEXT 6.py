3d7f76795b8e5a4193c83dca8bb28b30ac06924d
Liohana <142513545+Liohana@users.noreply.github.com>
2023-09-21 11:43:54 -0300

    Create ATV CONTEXT 6.py

---
+++ Liohana/Aula 6/ATV CONTEXT 6.py
@@ -0,0 +1,46 @@
+d#icionario = {
+#           "BR": [1,2,2,1,4,5,3],
+#            "USA": [3,4,56,3,6,4,2]
+#             }
+
+anestesias = {
+    "ketamina": [1kg = 1ml],
+    "xilazina": [1 kg = 0,5 ml],
+    "ketamina + xilazina": [1kg = 2 ml],
+    "halotanoG": [1kg = 10 ml]
+}
+
+an = input('anestesia')
+p = input('peso do animal')
+analisa dicionario
+print('aplique x ml de x')
+
+efeito = False
+pis = False
+
+#não está repetindo, posivelmente usar while
+if efeito == False:
+    print("espere fazer efeito")
+    efeito = input("fez efeito?")
+else:
+    print('posicione o animal')
+    if pis == False:
+        print('devido ao estimulo ele deve piscar')
+        pis = input('o animal piscou?')
+    else:
+        bregm = input('angulação bregma')
+        lambd = input('angulação lambda')
+        if bregm == lambd:
+            print('você tem uma superficie plana')
+        break
+        else:
+            print("ajuste o posicionamento do animal")
+
+#3 etapas
+  input('retirar pelagem')
+  input('retirar tecidos moles')
+  input('limpar calota')
+  if tudo ok? proxima etapa:
+    print('aplique o pliacrilato')
+
+V.
