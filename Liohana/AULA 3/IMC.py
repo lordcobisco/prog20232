c3d13286ecfd1108188b256adba43c80f8d5c119
Liohana <142513545+Liohana@users.noreply.github.com>
2023-09-27 11:21:48 -0300

    Update IMC.py

--- Liohana/AULA 3/IMC.py
+++ Liohana/AULA 3/IMC.py
@@ -12,17 +12,3 @@ print('Abaixo do peso normal', IMC >= 17 and IMC < 18.5)
 print('Peso dentro do normal', IMC >= 18.5 and IMC < 25)
 print('Acima do peso normal', IMC >= 25 and IMC < 30)
 print('Muito acima do peso normal', IMC >= 30)
-
-
-# Utilizando Estruturas de Decisão:
-
-if (IMC < 17):
-print('Você está muito abaixo do peso')
-if (IMC > 17 and IMC< 18.5):
-print('Você está abaixo do peso normal')
-if (IMC > 18.5 and IMC < 25):
-print('Você está no peso dentro do normal')
-if (IMC > 25 and IMC < 30):
-print('Você está acima do peso normal')
-if (IMC> 30):
-print('Você está muito acima do peso normal')
