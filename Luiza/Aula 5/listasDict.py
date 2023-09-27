e8e540e6b12d0ffced3289b5184950e94c11eb34
Luiza Araújo <141930552+luizaaraujoKL@users.noreply.github.com>
2023-09-26 21:27:09 -0300

    Update listasDict.py

--- Luiza/Aula 5/listasDict.py
+++ Luiza/Aula 5/listasDict.py
@@ -1,10 +1,13 @@
 listainterna = ["lista",1]
 compras = ["luiza", 1234, 1.2, listainterna,123]
 print(compras[3])
+
 compras[3] = 51
 print(compras [0 : len (compras):2])
+
 lista = []
 tupla = ()
+
 dicionario = {}
 dicionario ["professores"] = ["andre","denis"]
 dicionario["alunos"] = ["vitor"]
