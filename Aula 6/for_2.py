eaa9fdbe0ade0e6c6186cafaa0b2e1dd04312fe4
iasmimrocha <142513512+iasmimrocha@users.noreply.github.com>
2023-10-05 23:56:38 -0300

    Create for_2.py

---
+++ Aula 6/for_2.py
@@ -0,0 +1,8 @@
+#dado = [89127, 1298, 902, 3097, 356]
+soma = dado [0] + dado [1] + dado [2] + dado [3]
+soma = 0
+# for contador in range (len (dado)):
+#     soma = soma + (1 / frequenciaSensor) * dado [contador]
+for data in dado:
+    soma = soma + (1 / frequenciaSensor) * dado [contador]
+print (soma)
