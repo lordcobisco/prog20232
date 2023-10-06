8a53d18a8ebf8eb55371ced713f59fa96924ac9e
iasmimrocha <142513512+iasmimrocha@users.noreply.github.com>
2023-10-05 21:55:15 -0300

    Create tuplas.py

---
+++ Aula_5/tuplas.py
@@ -0,0 +1,7 @@
+tupla = ("azul", "amarelo", "lilás", "preto", "branco", "rosa", "marrom", "verde", "roxo", "vermelho")
+print(tupla[0:2])
+print(tupla[2:4])
+print(tupla[4:6])
+print(tupla[6:8])
+print(tupla[8:10])
+print(tupla[:])
