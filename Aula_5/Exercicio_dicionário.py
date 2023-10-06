f274f82c086d6dd115560a43b6b2d98508691dda
iasmimrocha <142513512+iasmimrocha@users.noreply.github.com>
2023-10-05 21:49:20 -0300

    Create Exercicio_dicionário.py

---
+++ Aula_5/Exercicio_dicionário.py
@@ -0,0 +1,4 @@
+dicionario = {'ISD' : ['Anita Garibaldi, IINELS'], 'professores' : ['lorenna, carol, denis, andré'], 'laboratórios' : ['neurobiologia, neuroprostetica, interface humano-máquina'] }
+print(dicionario['ISD'])
+print(dicionario['professores'])
+print(dicionario['laboratórios'])
