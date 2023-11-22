a7a9e2d2734bb52ea6c69c31d469633c2eb84b00
Luiza Araújo <141930552+luizaaraujoKL@users.noreply.github.com>
2023-11-22 19:42:21 -0300

    Update mqtt_ATIVIDADE.py

--- Luiza/Aula 9/mqtt_ATIVIDADE.py
+++ Luiza/Aula 9/mqtt_ATIVIDADE.py
@@ -1,26 +1,31 @@
 import paho.mqtt.client as mqtt
 import json
 
+import paho.mqtt.client as mqtt
+
 caminho = 'dadosSensor.txt'
 with open(caminho, 'w') as arquivo:
-    arquivo.write('Dados do Sensor IMU\n')
+    arquivo.write('Simulation Time: 3, Frequence: 20, Dados do IMU\n')
 
 def IMUdata():
     client.publish('cmd2dev9840',
                  '{"op":1,"simulationTime":3,'+
                  '"frequence":20,"sensorType":2}')
+    
+def stopIMUdata():
+    client.publish('dev9840ss', '{"op":22}')
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("newdev")
+    client.subscribe("dev9840ss")
     client.subscribe("dev9840status")
     IMUdata()
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic, ": ", msg.payload.decode('utf8'))
     if msg.topic == 'dev9840ss':
         with open(caminho, 'a') as arquivo:
-            arquivo.write(msg.payload+'\n')
+            arquivo.write(msg.payload.decode('utf8') + '\n')
 
 client = mqtt.Client()
 client.on_connect = on_connect
