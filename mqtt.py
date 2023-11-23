73afebd11a2d0a3fcaaf07320b063a0b03461605
mateusvillarroel <142513404+mateusvillarroel@users.noreply.github.com>
2023-11-23 00:11:56 -0300

    Arquivo MQTT atualizado

--- mqtt.py
+++ mqtt.py
@@ -1,31 +1,24 @@
 import paho.mqtt.client as mqtt
 import json
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
     client.subscribe("newdev")
-    client.subscribe("andre")
+    client.subscribe("dev9840ss")
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    client.publish('cmd2dev1488',
-                   '{"op":2,"m":"0,0,1,0",'+
-                   '"t":200,"p":20000,'+
-                   '"f":0}')
+    print(msg.topic+" "+(msg.payload.decode('utf8')))
+    client.publish('cmd2dev9840',
+                   '{"op":1,"simulationTime":"2","frequence":"2","sensorType":2}')
+    if msg.topic == "dev9840ss":
+        with open('mqttdados.txt', 'a') as f:
+            f.write(msg.payload.decode('utf-8'))
+        
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("10.1.0.18", 1883, 60)
+client.connect("0.tcp.sa.ngrok.io", 10618, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 client.loop_forever()
